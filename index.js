df1bf39d8564714604375ec79e552a0e997a1d8f
beaulazear <beaulazear@gmail.com>
2022-09-29 19:50:31 -0400

    commit tree 13467285751adec0fb9bdaaa52ce831236057f4a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, type) {
+    const newNames = [];
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+   return(newNames);
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
